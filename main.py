875dbf743b924cd6c8c9ad98a7ae885c2f0ad21c
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-04-27 22:35:22 -0700

    yeeeees

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from pdf2image import convert_from_path
 from multiprocessing import Pool
 
 import time
+import traceback
+
+import text
 
 def combine_pdf(file_int):
     merger = PdfFileMerger()
@@ -46,138 +49,102 @@ def do_ocr(file_int, paths):
         f.write(text)
     f.close()
 
-def validate_name(name):
-    if name == "":
-        return False
-
-    if not name.isupper():
-        return False
-
-    if not "," in name:
-        return False
-
-    return True
-
-def final_name_check(first_name, last_name):
-    if not last_name.isupper():
-        last_name = "ERROR"
-    else:
-        last_name = last_name.title()
-
-    if not first_name.isupper():
-        first_name = "ERROR"
-    else:
-        first_name = first_name.title()
-
-    bad_char_index = last_name.find("|")
-    if not bad_char_index == -1:
-        last_name = last_name[:bad_char_index] + last_name[bad_char_index+1:]
-
-    bad_char_index = first_name.find("|")
-    if not bad_char_index == -1:
-        first_name = first_name[:bad_char_index] + first_name[bad_char_index+1:]
-
-    return [last_name, first_name]
-
-def find_info(file_int):
-    outfile = "./text/out_text" + file_int + ".txt"
-    lines = []
-
-    with open(outfile) as f:
-        lines = [line.rstrip() for line in f]
-
-    dob_index = None
-
-    dob_indexes = [n for (n, e) in enumerate(lines) if e.startswith("DOB:")]
-
-    for dob_index in dob_indexes:
-        dob = lines[dob_index]
-        name = lines[dob_index-1]
-        # if this name isn't valid then try going to the line above it
-        if not validate_name(name):
-            name = lines[dob_index-2]
-            # if this is wrong too then try the next DOB index
-            if not validate_name(name):
-                continue
-            else:
-                break
-        else:
-            break
-
-    name = name.replace(" ", "")
-    dob = dob.replace(" ", "")
-    name_split = name.split(",")
-    last_name = name_split[0]
-    try:
-        first_name = name_split[1]
-    except Exception as e:
-        print("~~~---NAMING ISSUE HELP---~~~")
-        print(e)
-        print("~~~---ISSUE WITH: " + file_int + "---~~~")
-        last_name = "ERROR"
-        first_name = "ERROR"
-        return [dob, last_name, first_name]
-
-    last_name, first_name = final_name_check(first_name, last_name)
-
-    return [dob, last_name, first_name]
-
 def rename_file(file_int, data_list):
     current_file_name = "./combined/LastName_FirstName_DOB-YYYY-MM-DD_File-" + file_int + ".pdf"
 
-    dob = data_list[0]
-    year = str(19) + dob[10:]
-    month = dob[4:6]
-    date = dob[7:9]
+    name = data_list[0]
+    if name:
+        name_list = name.split(" ")
+        last_name = name_list[0].title()
+        first_name = name_list[1].title()
+    else:
+        last_name = "ERROR"
+        first_name = "ERROR"
 
-    last_name = data_list[1]
-    first_name = data_list[2]
+    dob = data_list[1]
+    if dob:
+        dob_list = dob.split("/")
+
+        year = dob_list[2]
+        year = "19" + year
+
+        month = dob_list[0]
+        if len(month) == 1:
+            month = "0" + month
+
+        date = dob_list[1]
+        if len(date) == 1:
+            date = "0" + date
+    else:
+        year = "YYYY"
+        month = "MM"
+        date = "DD"
 
     new_file_name = f"./combined/{last_name}_{first_name}_DOB-{year}-{month}-{date}_File-{file_int}.pdf"
 
-    os.rename(current_file_name, new_file_name)
+    try:
+        os.rename(current_file_name, new_file_name)
+    except FileExistsError:
+        print("\nERR: Could not rename file for: " + str(file_int))
+        print("File already seems to exist..: " + new_file_name)
+        print("Deleting new file made..\n")
+
+        os.remove(current_file_name)
 
 def do_thing(file_int):
     file_int = str(file_int)
 
     temp_file_path = combine_pdf(file_int)
-    print("finished combining: " + file_int)
+    print("done combining, saving imgs: " + file_int)
 
     paths = convert_pdf_image(temp_file_path, file_int)
-    print("finished converting and saving imgs: " + file_int)
+    print("done saving imgs, doing ocr: " + file_int)
 
     do_ocr(file_int, paths)
-    print("ocr done, finding data: " + file_int)
+    print("done ocr, finding data: " + file_int)
 
-    data_list = find_info(file_int)
+    data_list = text.find_info(file_int)
     print("found data, renaming file: " + file_int)
 
     rename_file(file_int, data_list)
 
     print("ALL DONE FOR: " + file_int)
+    return file_int
 
+def main():
+    path = "./files/"
+    files = [f for f in os.listdir(path)]
 
-if __name__ == "__main__":
-    pool = Pool(10)
-
-    # inclusive on start, EXCLUSIVE on the end
-    start = 5450
-    end = 5550
-
+    pool = Pool(6)
     start_time = time.time()
 
+    done = []
+
     try:
-        pool.map(do_thing,(x for x in range(start, end)))
-        # pool.map(do_thing,(x for x in nums))
+        done = pool.map(do_thing,(x for x in files))
         pool.close()
         pool.join()
     except KeyboardInterrupt:
         pool.terminate()
         sys.exit()
-    except Exception as e:
-        print(e)
+    except Exception:
+        print(traceback.format_exc())
+
+    for f in done:
+        if not f:
+            continue
+        if f in files:
+            files.remove(f)
+
+    print("~ ~ not done for some reason: ")
+    print(files, end = "~ ~ \n\n")
 
     print("--- %s seconds ---" % (time.time() - start_time))
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
 
 ##
 # we should also detect the "DOB/AGE/" part too
